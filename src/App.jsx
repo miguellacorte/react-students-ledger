68ceb741961251d173145e8c59e3571a6a9e7687
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2022-07-16 16:18:23 +0200

    iteration 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,140 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import users from "./users";
+import 'bootstrap/dist/css/bootstrap.css';
+import linkPic from "./linkedin.png"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // console.log(users)
+  
+  const [name,setName] = useState('');
+  const [search,setSearch] = useState('');
+  const [userList, setUserList] = useState(users)
+  const [studentCheck,setStudentCheck] = useState(false);
+  const [teacherCheck,setTeacherCheck] = useState(false);
+  
+  // console.log(search.toLowerCase())
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newSearch = {search};
+    console.log("Submitted:",newSearch );
+  
+  }
+
+  const filteredUsers = userList.filter(user =>{
+    if (user.lastName.toLowerCase().includes(search.toLowerCase()) || user.firstName.toLowerCase().includes(search.toLowerCase()) ){
+      return true
+    } else {
+      return false
+    }
+  });
+
+ 
+  const handleIsStudentChange = e => {
+    setStudentCheck(e.target.checked)
+
+    if (studentCheck === true) {
+      setUserList(users)
+     } else {
+       const checkboxFunct =  userList.filter(user => 
+         user.role === 'student')
+        setUserList(checkboxFunct);
+     }
+
+  }
+
+  const handleIsTeacherChange = e => {
+    setTeacherCheck(e.target.checked)
+    console.log(e.target.checked)
+
+    if (teacherCheck === true) {
+      setUserList(users)
+     } else {
+       const checkboxFunct =  userList.filter(user => 
+         user.role === 'teacher')
+        setUserList(checkboxFunct);
+     }
+
+  }
+  
+   
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+
+    <form onSubmit={handleSubmit} >
+      <label> Search:</label>
+      <input 
+      type="text"
+      value={search}
+      placeholder="Search by name"
+      name="input" 
+      onChange = {(e)=> setSearch(e.target.value)}
+      />
+
+      <label>
+        <input 
+        type="checkbox" 
+        value='Student'
+        checked={studentCheck}
+        onChange = {handleIsStudentChange}
+        />
+        Students
+      </label>  
+
+      <label>
+        <input 
+        type="checkbox" 
+        value='teacher'
+        checked={teacherCheck}
+        onChange = {handleIsTeacherChange}
+        />
+        Teachers
+      </label>  
+
+
+      
+
+    </form>
+    
+
+    <div className='Table'>
+    <table> 
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Campus</th>
+          <th>Role</th>
+          <th>Links</th>
+        </tr>
+      </thead>
+
+    <tbody>
+    
+      {filteredUsers.map(user => (
+          <tr key={user.id}>
+            <td>{user.firstName}</td>
+            <td>{user.lastName}</td>
+            <td>{user.campus}</td>
+            <td>{user.role}</td>
+            <td>{user.linkedin ? <img height="16" src={linkPic}/> : ""}</td>
+          </tr>
+        ))}
+        
+      </tbody>
+    </table>
     </div>
-  )
+   
+    </div>
+ 
+
+  );
 }
 
 export default App
